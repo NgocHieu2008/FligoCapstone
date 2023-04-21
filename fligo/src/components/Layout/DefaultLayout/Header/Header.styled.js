6ffe912afc6fba2a8ea167948d442b90a177c298
Ngoc Hieu <tnngochieu2008@gmail.com>
2023-04-21 23:40:52 +0700

    login signup

--- fligo/src/components/Layout/DefaultLayout/Header/Header.styled.js
+++ fligo/src/components/Layout/DefaultLayout/Header/Header.styled.js
@@ -5,7 +5,14 @@ export const Wrapper = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin: 20px 15px;
+  position: relative;
+  z-index: 1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  z-index: 9999; /* Đảm bảo header luôn nằm trên top nhất */
 `;
 
 export const NavbarDiv = styled.div`
@@ -30,7 +37,7 @@ export const NavbarItem = styled.li`
   margin: 0 30px;
 `;
 
-export const Link = styled.a`
+export const Linked = styled.a`
   text-decoration: none;
   color: #0e185f;
   font-weight: bolder;
@@ -75,3 +82,139 @@ export const Button = styled.button`
   }
 `;
 
+export const RegisterButton = styled.button`
+  background-color: var(--blue-secondary-color);
+  text-align: center;
+  padding: 5px;
+  outline: none;
+  border: none;
+  color: #fff;
+  width: 90px;
+  height: 30px;
+  font-weight: bolder;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    color: var(--blue-secondary-color);
+    background-color: #fff;
+    outline: 1px solid var(--blue-secondary-color);
+    & ${Icon} path {
+      fill: var(--blue-secondary-color);
+    }
+  }
+`;
+
+export const LoginButton = styled.button`
+  background-color: var(--blue-primary-color);
+  text-align: center;
+  padding: 5px;
+  outline: none;
+  border: none;
+  color: #fff;
+  width: 90px;
+  height: 30px;
+  font-weight: bolder;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    color: var(--blue-primary-color);;
+    background-color: #fff;
+    outline: 1px solid var(--blue-primary-color);;
+    & ${Icon} path {
+      fill: var(--blue-primary-color);;
+    }
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  gap: 10px;
+  `;
+
+  export  const FormWrapper = styled.div`
+  position: absolute;
+  top: 80%;
+  right: 45px;
+  min-width: 240px;
+  background-color: #fff;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+`;
+
+export const FormHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  border-radius: 5px 5px 0 0;
+  background-color: var(--blue-primary-color);
+  color: #fff;
+`;
+
+export const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 20px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Label = styled.label`
+  margin: 10px 0;
+  font-weight: bold;
+  color: var(--blue-secondary-color);
+  font-size: 1.25rem;
+`;
+
+export const Input = styled.input`
+    width: 100%;
+    height: 40px;
+    outline: none;
+    border: none;
+    border-radius: 3px;
+    box-shadow: 0px 4px 4px #c4c4c4;
+    border: 0.4px solid #c4c4c4;
+    margin-bottom: 10px;
+    padding: 8px;
+    font-size: 16px;
+`;
+
+export const LoginButtonForm = styled.button`
+  background-color: var(--green-primary-color);
+  text-align: center;
+  padding: 5px;
+  outline: none;
+  border: none;
+  color: #fff;
+  width: 90px;
+  height: 30px;
+  font-weight: bolder;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0;
+  &:hover {
+    color: var(--green-primary-color);;
+    background-color: #fff;
+    outline: 1px solid var(--green-primary-color);;
+  }
+`;
+
+export const ErrorMessage = styled.p`
+  color: red;
+`;
+
+export const Br = styled.br`
+  display: block;
+  height: 20px;
+`;
--- fligo/src/pages/SignUp/SignUp.jsx
+++ fligo/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,150 @@
-function SignUp() {
-    return ( 
-        <h1>Sign up</h1>
-     );
-}
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+import { Wrapper, DivStyled, DivTitle, DivForm, FormStyled, LabelStyled, InputStyled, FieldWrapper, ErrorStyled, DateContainer, ButtonStyled } from './SignUp.styled';
+
+const SignupSchema = Yup.object().shape({
+    userName: Yup.string().required('Required').min(6, 'Username must contain at least 6 characters'),
+    firstName: Yup.string().required('Required'),
+    lastName: Yup.string().required('Required'),
+    dayOfBirth: Yup.number().required('Required').positive().integer(),
+    monthOfBirth: Yup.number().required('Required').max(12),
+    yearOfBirth: Yup.number().required("Required").positive().integer().max(new Date().getFullYear() - 18),
+    passportNumber: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string()
+        .required('Required')
+        .min(8, 'Password must contain at least 8 characters'),
+    confirmPassword: Yup.string()
+        .required('Required')
+        .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+});
+
+const SignUp = () => {
+  const handleSubmit = (values) => {
+    console.log(values);
+    const id = Date.now().toString();
+    const dateOfBirth = moment(`${values.yearOfBirth}-${values.monthOfBirth}-${values.dayOfBirth}`).format('YYYY-MM-DD');
+    const user = {
+        id,
+        userName: values.userName,
+        firstName: values.firstName,
+        lastName: values.lastName,
+        dateOfBirth,
+        passportNumber: values.passportNumber,
+        email: values.email,
+        password: values.password,
+    }
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    // check if user already exists
+    const userExists = users.find((user) => user.email === values.email);
+    if (userExists) {
+        alert('User already exists');
+        return;
+    }
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users));
+    window.location.href = '/';
+  };
+  return (
+    <Wrapper>
+        <DivTitle>Create your new account</DivTitle>
+        <DivStyled>
+        <Formik
+        initialValues={{
+          userName: '',
+          firstName: '',
+          lastName: '',
+          dayOfBirth: '',
+          monthOfBirth: '',
+          yearOfBirth: '',
+          passportNumber: '',
+          email: '',
+          password: '',
+          confirmPassword: '',
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ errors, touched, values, setFieldValue }) => (
+          <Form>
+            <div>
+              <label htmlFor="userName">User Name</label>
+              <InputStyled name="userName" />
+              <ErrorMessage name="userName" />
+            </div>
+            <div>
+              <label htmlFor="firstName">First Name</label>
+              <FieldWrapper>
+                  <InputStyled name="firstName" />
+                  <ErrorMessage name="firstName" />
+              </FieldWrapper>
+            </div>
+
+            <div>
+              <label htmlFor="lastName">Last Name</label>
+              <InputStyled name="lastName" />
+              <ErrorMessage name="lastName" />
+            </div>
+
+            <DateContainer>
+                <div>
+                  <label htmlFor="monthOfBirth">Month of Birth</label>
+                  <InputStyled
+                    name="monthOfBirth"
+                    type="number" placeholder="Month" min="1" max="12"
+                  >
+                  </InputStyled>
+                  <ErrorMessage name="monthOfBirth" />
+                </div>
+                <div>
+                  <label htmlFor="yearOfBirth">Year of Birth</label>
+                  <InputStyled
+                    min="1900" max={new Date().getFullYear() - 18}
+                    name="yearOfBirth"
+                    type="number"
+                  />
+                  <ErrorMessage name="yearOfBirth" />
+                </div>
+                <div>
+                  <label htmlFor="dayOfBirth">Day of Birth</label>
+                  <InputStyled name="dayOfBirth" type="number" placeholder="Day" min="1" max="31" >
+                  </InputStyled>
+                  <ErrorMessage name="dayOfBirth" />
+                </div>
+            </DateContainer>
+            <div>
+              <label htmlFor="passportNumber">Passport Number</label>
+              <InputStyled name="passportNumber" />
+              <ErrorMessage name="passportNumber" />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <InputStyled name="email" type="email" />
+              <ErrorMessage name="email" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <InputStyled name="password" type="password" />
+              <ErrorMessage name="password" />
+            </div>
+            <div>
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <InputStyled name="confirmPassword" type="password" />
+              <ErrorMessage name="confirmPassword" />
+            </div>
+            
+            <p>By clicking SIGN UP, I agree that I have read and accepted the Terms of use and Privacy Policy of FLIGO</p>
+            <p><span>Already have an account? </span><Link to="/">Login</Link></p>
+            <ButtonStyled type="submit">Sign Up</ButtonStyled>
+          </Form>
+        )}
+      </Formik>
+        </DivStyled>
+    </Wrapper>
+  );
+};
 
 export default SignUp;
