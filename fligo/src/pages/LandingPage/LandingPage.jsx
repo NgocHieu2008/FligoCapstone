42ebd30e7f40a34e9de2ad9c79018fea49695442
NgocHieu2008 <98446142+NgocHieu2008@users.noreply.github.com>
2023-04-21 12:34:45 +0700

    Feature/hieu (#5)
    * install carousel
    
    * landing page
    
    UI landing page

--- fligo/src/pages/LandingPage/LandingPage.jsx
+++ fligo/src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,43 @@
+import BannerSlider from "~/components/Carousels/BannerSlider/BannerSlider";
+import { LandingPageContainer,FirstSessionContainer, SessionTitle, SecondSessionContainer, SubSession, Item, FlightPopularContainer } from "./LandingPage.styled";
+import { PopularFlight } from '~/data/PopularFlight';
+import PopularFlightCard from '~/components/Cards/PopularFlightCard/PopularFlightCard';
+
+
 function LandingPage() {
     return ( 
-        <h1>LandingPage</h1>
+        <LandingPageContainer>
+            <BannerSlider />
+            <FirstSessionContainer>
+                <SessionTitle>Explore Popular Flight</SessionTitle>
+                <FlightPopularContainer>
+                    {PopularFlight.map((flight, index) => (
+                        <PopularFlightCard key={index} flight={flight} />
+                    ))}
+                </FlightPopularContainer>
+            </FirstSessionContainer>
+            <SecondSessionContainer>
+                <SessionTitle>Additional Service</SessionTitle>
+                <SubSession>
+                    <Item>
+                        <img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/29/57/12.jpg" alt="img" />
+                        <h3>Prepaid Baggage</h3>
+                    </Item>
+                    <Item>
+                        <img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/29/57/12.jpg" alt="img" />
+                        <h3>Flight Delay/Cancelation</h3>
+                    </Item>
+                    <Item>
+                        <img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/29/57/12.jpg" alt="img" />
+                        <h3>Meet & Greet Service</h3>
+                    </Item>
+                    <Item>
+                        <img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/29/57/12.jpg" alt="img" />
+                        <h3>Paid Meal Service</h3>
+                    </Item>
+                </SubSession>
+            </SecondSessionContainer>
+        </LandingPageContainer>
      );
 }
 
