8cac298512fbc3daac703d248e1e8cfbae37274e
Sarah Nguyen <91973283+sarahnguyenS2@users.noreply.github.com>
2023-05-08 14:05:29 +0700

    Feature/hao (#27)
    * Add header component
    
    * Add Forgot password & Reset password page
    
    * Fix verify OTP code
    
    * Update new server and fix verify function
    
    * Update new server
    
    * Update reset password
    
    * Add info booking and success payment page
    
    * Add info booking and success payment page
    
    * Map front end with api
    
    * Solve conflict
    
    * Change link server

--- fligo/src/components/Cards/HistoryCard/HistoryCard.jsx
+++ fligo/src/components/Cards/HistoryCard/HistoryCard.jsx
@@ -1,4 +1,4 @@
-import {Wrapper,Img} from "./HistoryCard.styled";
+import { Wrapper, Img, TicketWrapper } from "./HistoryCard.styled";
 import wallet from "~/assets/Wallet_alt.png";
 import user from "~/assets/user.png";
 import ticket from "~/assets/Ticket_use.png";
@@ -7,52 +7,109 @@ import date from "~/assets/Date_today_light.png";
 import flight from "~/assets/flight_icon.png";
 import vietjet from "~/assets/vietjet-air-logo.png";
 import vietnamairline from "~/assets/vietnam-airline-logo.png";
-import { useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { UserContext } from "~/contexts/UserContext";
 
 function HistoryCard() {
-
-    const { userData } = useContext(UserContext);
-    console.log(userData);
-    return ( 
-        <Wrapper>
+  const { userData } = useContext(UserContext);
+  const [bookingInfo, setBookingInfo] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://fligo.vercel.app/tickets/${userData.username}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log("hello");
+      //   console.log(data.tickets);
+      const bookedInfo = Object.values(data);
+      //   const transformedData = bookingInfo.map((ticket) => ({
+      //     reservationCode: ticket.reservationCode,
+      //     fullname: `${ticket.firstname} ${ticket.lastname}`,
+      //     bookedDate: new Date().toLocaleDateString("en-US", {
+      //       day: "numeric",
+      //       month: "short",
+      //       year: "numeric",
+      //     }),
+      //     // add any other fields you need here
+      //   }));
+      const transformedData = bookedInfo.map((ticket) => ({
+        departure_time: ticket.departure_time,
+        arrival_time: ticket.arrival_time,
+        reservationCode: ticket.reservationCode,
+        firstname: ticket.firstname,
+        lastname: ticket.lastname,
+        bookedDate: new Date().toLocaleDateString("en-US", {
+          day: "numeric",
+          month: "short",
+          year: "numeric",
+        }),
+        status: "Paid",
+        total: ticket.price,
+      }));
+      setBookingInfo(transformedData.flat());
+      console.log(transformedData);
+    }
+    fetchData();
+  }, []);
+  return (
+    <>
+      <TicketWrapper>
+        {bookingInfo.map((booking) => (
+          <Wrapper key={booking.reservationCode}>
             <div>
-                <p>
-                    <Img src={flight} alt="flight"/>
-                    SGN - HAN
-                </p>
-                <p>
-                    <Img src={ticket} alt="ticket"/>
-                    Reversation code
-                </p>
-                <p>
-                    <Img src={user} alt="user"/>
-                    Passenger name
-                </p>
-                <p>
-                    <Img src={date} alt="date"/>
-                    Booking date
-                </p>
-                <p>
-                    <Img src={check} alt="check"/>
-                    Status
-                </p>
-                <p>
-                    <Img src={wallet} alt="wallet"/>
-                    Total
-                </p>
+              <p>
+                <Img src={flight} alt="flight" />
+                SGN - HAN
+              </p>
+              <p>
+                <Img src={ticket} alt="ticket" />
+                Reversation code
+              </p>
+              <p>
+                <Img src={user} alt="user" />
+                Passenger name
+              </p>
+              <p>
+                <Img src={date} alt="date" />
+                Booking date
+              </p>
+              <p>
+                <Img src={check} alt="check" />
+                Status
+              </p>
+              <p>
+                <Img src={wallet} alt="wallet" />
+                Total
+              </p>
             </div>
-            <img src={vietjet} alt="airline" style={{width:"50px"}}/>
-            <div style={{display:"flex", flexDirection:"column"}}>
-                <span>20:00 - 22:00</span>
-                <span>123456789</span>
-                <span>Nguyen Van A</span>
-                <span>20/10/2021</span>
-                <span>Paid</span>
-                <span>$234</span>
+            <img src={vietjet} alt="airline" style={{ width: "50px" }} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "fit-content",
+              }}
+            >
+              <span>{booking.departure_time} - {booking.arrival_time}</span>
+              <span>{booking.reservationCode}</span>
+              <span>
+                {booking.firstname} {booking.lastname}
+              </span>
+              <span>{booking.bookedDate}</span>
+              <span>{booking.status}</span>
+              <span>{booking.total}</span>
             </div>
-        </Wrapper>
-     );
+          </Wrapper>
+        ))}
+      </TicketWrapper>
+    </>
+  );
 }
 
 export default HistoryCard;
--- fligo/src/pages/Payment/Visa.jsx
+++ fligo/src/pages/Payment/Visa.jsx
@@ -1,10 +1,20 @@
 import { Formik } from "formik";
 import * as Yup from "yup";
-import {FormStyled, FieldWrapper, Label, Input, Error, DetailWrapper, SubmitButton, } from "./Payment.styled";
+import {
+  FormStyled,
+  FieldWrapper,
+  Label,
+  Input,
+  Error,
+  DetailWrapper,
+  SubmitButton,
+} from "./Payment.styled";
 const visaSchema = Yup.object().shape({
-    cardNumber: Yup.string().required("Required"),
-    cardHolder: Yup.string().required("Required"),
-    expiryDate: Yup.string().required("Required").matches(/^[0-9]{2}\/[0-9]{2}$/, "Expiry date is not valid") // Kiểm tra chuỗi có đúng định dạng "MM/YY" hay không
+  cardNumber: Yup.string().required("Required"),
+  cardHolder: Yup.string().required("Required"),
+  expiryDate: Yup.string()
+    .required("Required")
+    .matches(/^[0-9]{2}\/[0-9]{2}$/, "Expiry date is not valid") // Kiểm tra chuỗi có đúng định dạng "MM/YY" hay không
     .test({
       name: "expiryDate",
       test: (value) => {
@@ -14,90 +24,136 @@ const visaSchema = Yup.object().shape({
         expiryDate.setMonth(expiryDate.getMonth() + 1); // Cộng thêm 1 đơn vị tháng, để lấy ngày cuối cùng của tháng
         return expiryDate > currentDate; // Kiểm tra xem ngày hết hạn có sau ngày hiện tại hay không
       },
-      message: "Card has expired"
+      message: "Card has expired",
     }),
-    cvv: Yup.string().required("Required").length(4, "Invalid CVV"),
+  cvv: Yup.string().required("Required").length(4, "Invalid CVV"),
 });
 
 function Visa() {
+  const paymentDetails = JSON.parse(localStorage.getItem("bookingInfo"));
+  const flight = JSON.parse(localStorage.getItem("selectedFlight"));
+  const seat = JSON.parse(localStorage.getItem("selectedSeat"));
+  const passenger = JSON.parse(localStorage.getItem("passengerInfo"));
+  const handleSubmit = async (values) => {
+    // Lưu thông tin thanh toán vào local storage
+    localStorage.setItem("paymentInfo", JSON.stringify(values));
+    const response = await fetch("https://fligo.vercel.app/book-seat", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        flight_number: flight.flight_number,
+        seat: seat.row + seat.column,
+        firstname: passenger.firstname,
+        lastname: passenger.lastname,
+        dateOfBirth: `${passenger.yearOfBirth}-${passenger.monthOfBirth}-${passenger.dayOfBirth}`,
+        passport: passenger.passport,
+        expiryDate: `${passenger.yearExpire}-${passenger.monthExpire}-${passenger.dayExpire}`,
+        title: passenger.title,
+        nationality: passenger.nationality,
+        paymentBill:
+          paymentDetails.flightPrice +
+          paymentDetails.insurancePrice +
+          paymentDetails.serviceFee,
+      }),
+    });
+    // Chuyển qua trang thanh toán thành công
+    if (response.ok) {
+      // localStorage.removeItem("passengerInfo");
+      localStorage.removeItem("selectedSeat");
+      // localStorage.removeItem("selectedFlight");
+      // localStorage.removeItem("bookingInfo");
+      window.location.href = "/success-payment";
+    }
+  };
 
-    const paymentDetails = JSON.parse(localStorage.getItem("bookingInfo"));
-    const flight = JSON.parse(localStorage.getItem("selectedFlight"));
-
-    const handleSubmit = (values) => {
-        // Lưu thông tin thanh toán vào local storage
-        localStorage.setItem("paymentInfo", JSON.stringify(values));
-        // Chuyển qua trang thanh toán thành công
-        window.location.href = "/success-payment";
-    };
-
-    return ( 
-        <>
-        <Formik
-            initialValues={{
-                cardNumber: "",
-                cardHolder: "",
-                expiryDate: "",
-                cvv: "",
-            }}
-            validationSchema={visaSchema}
-            onSubmit={handleSubmit}
-        >
-            {({}) => (
-                <FormStyled>
-                    <FieldWrapper>
-                        <Label htmlFor="cardNumber">Card number</Label>
-                        <Input type="text" name="cardNumber" id="cardNumber" />
-                        <Error name="cardNumber" component="div" />
-                    </FieldWrapper>
-                    <FieldWrapper>
-                        <Label htmlFor="cardHolder">Card holder</Label>
-                        <Input type="text" name="cardHolder" id="cardHolder" />
-                        <Error name="cardHolder" component="div" />
-                    </FieldWrapper>
-                    <div style={{display:"inline-flex"}}>
-                        <FieldWrapper>
-                            <Label htmlFor="expiryDate">Expiry date (MM/YY)</Label>
-                            <Input type="text" name="expiryDate" id="expiryDate" />
-                            <Error name="expiryDate" component="div" />
-                        </FieldWrapper>
-                        <FieldWrapper style={{marginLeft:"20px"}}>
-                            <Label htmlFor="cvv">CVV</Label>
-                            <Input type="text" name="cvv" id="cvv" />
-                            <Error name="cvv" component="div" />
-                        </FieldWrapper>
-                    </div>
-                    <DetailWrapper>
-                        <h3>DETAILS</h3>
-                        <div style={{display:"flex", justifyContent:"space-between"}}>
-                            <div>
-                                <p>{flight.airline} (Adult) x1</p>
-                                <p>Travel Insurance</p>
-                                <p>Service Fee</p>
-                            </div>
-                            <div>
-                                <p style={{textAlign:"end"}}>${paymentDetails.flightPrice}</p>
-                                <p style={{textAlign:"end"}}>${paymentDetails.insurancePrice}</p>
-                                <p style={{textAlign:"end"}}>${paymentDetails.serviceFee}</p>
-                            </div>
-                        </div>
-                        <div style={{borderBottom:"1px solid #000", marginBottom: "10px"}}></div>
-                        <div style={{display:"flex", justifyContent:"space-between"}}>
-                            <div>
-                                <h3>Total</h3>
-                            </div>
-                            <div>
-                                <h3 style={{textAlign:"end"}}>${paymentDetails.flightPrice + paymentDetails.insurancePrice + paymentDetails.serviceFee}</h3>
-                            </div>
-                        </div>
-                    </DetailWrapper>
-                    <p style={{fontSize:"14px", color:"#1C2668", fontWeight:"600"}}>By clicking Pay Now, I agree that I have read and accepted the <span style={{color:"#2FA4FF"}}>Terms and Conditions</span> of FLIGO</p>
-                    <SubmitButton type="submit">Pay Now</SubmitButton>
-                </FormStyled>
-            )}
-        </Formik>
-        </>
-     );
+  return (
+    <>
+      <Formik
+        initialValues={{
+          cardNumber: "",
+          cardHolder: "",
+          expiryDate: "",
+          cvv: "",
+        }}
+        validationSchema={visaSchema}
+        onSubmit={handleSubmit}
+      >
+        {({}) => (
+          <FormStyled>
+            <FieldWrapper>
+              <Label htmlFor="cardNumber">Card number</Label>
+              <Input type="text" name="cardNumber" id="cardNumber" />
+              <Error name="cardNumber" component="div" />
+            </FieldWrapper>
+            <FieldWrapper>
+              <Label htmlFor="cardHolder">Card holder</Label>
+              <Input type="text" name="cardHolder" id="cardHolder" />
+              <Error name="cardHolder" component="div" />
+            </FieldWrapper>
+            <div style={{ display: "inline-flex" }}>
+              <FieldWrapper>
+                <Label htmlFor="expiryDate">Expiry date (MM/YY)</Label>
+                <Input type="text" name="expiryDate" id="expiryDate" />
+                <Error name="expiryDate" component="div" />
+              </FieldWrapper>
+              <FieldWrapper style={{ marginLeft: "20px" }}>
+                <Label htmlFor="cvv">CVV</Label>
+                <Input type="text" name="cvv" id="cvv" />
+                <Error name="cvv" component="div" />
+              </FieldWrapper>
+            </div>
+            <DetailWrapper>
+              <h3>DETAILS</h3>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  <p>{flight.airline} (Adult) x1</p>
+                  <p>Travel Insurance</p>
+                  <p>Service Fee</p>
+                </div>
+                <div>
+                  <p style={{ textAlign: "end" }}>
+                    ${paymentDetails.flightPrice}
+                  </p>
+                  <p style={{ textAlign: "end" }}>
+                    ${paymentDetails.insurancePrice}
+                  </p>
+                  <p style={{ textAlign: "end" }}>
+                    ${paymentDetails.serviceFee}
+                  </p>
+                </div>
+              </div>
+              <div
+                style={{ borderBottom: "1px solid #000", marginBottom: "10px" }}
+              ></div>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  <h3>Total</h3>
+                </div>
+                <div>
+                  <h3 style={{ textAlign: "end" }}>
+                    $
+                    {paymentDetails.flightPrice +
+                      paymentDetails.insurancePrice +
+                      paymentDetails.serviceFee}
+                  </h3>
+                </div>
+              </div>
+            </DetailWrapper>
+            <p
+              style={{ fontSize: "14px", color: "#1C2668", fontWeight: "600" }}
+            >
+              By clicking Pay Now, I agree that I have read and accepted the{" "}
+              <span style={{ color: "#2FA4FF" }}>Terms and Conditions</span> of
+              FLIGO
+            </p>
+            <SubmitButton type="submit">Pay Now</SubmitButton>
+          </FormStyled>
+        )}
+      </Formik>
+    </>
+  );
 }
 
 export default Visa;
